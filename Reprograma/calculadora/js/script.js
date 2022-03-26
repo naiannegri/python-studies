814c8e68618d109791764ac8260f21033aa7636b
Naiane Negri <naiannegri@gmail.com>
2022-03-26 14:28:39 -0300

    remove js

--- Reprograma/calculadora/js/script.js
+++
@@ -1,6 +0,0 @@
-function calcularValorHora() {
-    const valorMes = document.querySelector('#ganho-mes')
-    const horas = document.querySelector('#horas-dia')
-    const horasMes = horas.value * 22
-    resposta.innerHTML = 'R$ ' + parseFloat(valorMes.value/horasMes).toFixed(2)
-}
